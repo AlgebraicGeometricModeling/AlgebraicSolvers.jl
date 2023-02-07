f7c333da72d652b17661bba5cd1a38872c698133
mourrain <bernard.mourrain@inria.fr>
2023-02-07 12:44:16 +0100

    update

---
+++ docs/make.jl
@@ -0,0 +1,26 @@
+using Documenter, AlgebraicSolvers
+
+dir="mrkd"
+Expl = map(file -> joinpath("expl", file), filter(x ->endswith(x, "md"), readdir(dir*"/expl")))
+Code = map(file -> joinpath("code", file), filter(x ->endswith(x, "md"), readdir(dir*"/code")))
+
+makedocs(
+         sitename = "AlgebraicSolvers",
+         authors = "B. Mourrain",
+         modules = [AlgebraicSolvers],
+         build = "AlgebraicSolvers.jl/docs",
+         source = dir,
+         pages = Any[
+                     "Home" => "index.md",
+                     "Examples" => Expl,
+                     "Functions" => Code
+                     ],
+         repo = "https://github.com/AlgebraicGeometricModeling/AlgebraicSolvers.jl",
+         doctest = false
+         )
+
+deploydocs(
+           repo = "github.com/AlgebraicGeometricModeling/AlgebraicSolvers.jl.git",
+           target = "site"
+           )
+
